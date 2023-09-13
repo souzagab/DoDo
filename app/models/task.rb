1986558103daad9b63177f2a64b111e579e6cfaa
Gabriel S <thesouzagab@gmail.com>
2023-09-12 23:48:26 -0300

    chore: rubocop -A :broom:

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
 
-  broadcasts_to ->(task) { :tasks }, inserts_by: :prepend
+  broadcasts_to ->(_task) { :tasks }, inserts_by: :prepend
 
   validates :body, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Dodo
     config.load_defaults 7.1
 
     # Please, see https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#config-autoload-lib-ignore.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
