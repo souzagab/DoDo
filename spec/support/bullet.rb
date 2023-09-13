f37df9703514c3508a2a3fe274be2936e2be0004
Gabriel S <thesouzagab@gmail.com>
2023-09-12 23:50:43 -0300

    spec: add rspec and helpers

---
+++ spec/support/bullet.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.around do |example|
+    # config tag to not use bullet
+    without_bullet = example.metadata[:without_bullet]
+
+    Bullet.enable = false if without_bullet
+
+    example.run
+
+    Bullet.enable = true if without_bullet
+  end
+end
