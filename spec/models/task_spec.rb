af3ba9d6e74e5934a808c3526b7f5e3960a269b3
Gabriel S. <thesouzagab@gmail.com>
2023-09-15 23:13:11 -0300

    spec: add search test and task factory

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,23 @@
-RSpec.describe Task, type: :model do
-  it { is_expected.to validate_presence_of(:body) }
+RSpec.describe Task do
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:body) }
+  end
+
+  describe ".search" do
+    let!(:task) { create(:task, body: "foo") }
+    let!(:other_task) { create(:task, body: "bar") }
+    let!(:another_task) { create(:task, body: "baz") }
+
+    let(:all_tasks) { [task, other_task, another_task] }
+    let(:search_params) { { q: "ba" }.with_indifferent_access }
+
+    it "returns all tasks when no search params are given" do
+      expect(described_class.search({})).to match_array(all_tasks)
+    end
+
+    it "returns tasks matching the search params" do
+      expect(described_class.search(search_params)).to contain_exactly(other_task, another_task)
+    end
+  end
 end
