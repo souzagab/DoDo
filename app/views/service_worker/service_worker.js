c9a1b8f7be61e86d255994d799f6399519bf0b9f
Gabriel S. <thesouzagab@gmail.com>
2023-09-18 18:30:10 -0300

    feat: add cache support with google's workbox

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -1,3 +1,26 @@
+
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox-sw.js"
+)
+
+const { CacheFirst, NetworkFirst } = workbox.strategies
+const { registerRoute } = workbox.routing
+
+registerRoute(
+    ({ request }) => request.destination === 'image',
+    new CacheFirst({ cacheName: 'images' })
+)
+
+registerRoute(
+    ({ request }) => request.destination === 'script' || request.destination === 'style',
+    new CacheFirst({ cacheName: 'assets' })
+)
+
+registerRoute(
+    ({ request }) => request.destination === 'document',
+    new NetworkFirst({ cacheName: 'pages' })
+)
+
 function onInstall(event) {
     console.log('[Service-worker]', "Installing!", event);
 }
