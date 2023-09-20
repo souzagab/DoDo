bed2d1abce65fb698efd749065e9dfa2ccb3c223
Gabriel S. <thesouzagab@gmail.com>
2023-09-20 18:12:48 -0300

    feat: add authentication helper methods on base controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :set_current_request_details
   before_action :authenticate
 
+  helper_method :user_signed_in?, :current_user, :current_session
+
   private
+
+  def current_user
+    Current.user
+  end
+
+  def current_session
+    Current.session
+  end
+
+  def user_signed_in?
+    Current.user.present?
+  end
 
   def authenticate
     if (session_record = Session.find_by(id: cookies.signed[:session_token]))
