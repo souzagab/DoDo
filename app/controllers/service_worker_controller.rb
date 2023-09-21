3f5ee385ce1f4f45ab68d18716b21b512199badf
Gabriel S. <thesouzagab@gmail.com>
2023-09-21 20:10:41 -0300

    feat: skip authentication on service worker endpoint

--- app/controllers/service_worker_controller.rb
+++ app/controllers/service_worker_controller.rb
@@ -1,5 +1,6 @@
 class ServiceWorkerController < ApplicationController
   protect_from_forgery except: :service_worker
+  skip_before_action :authenticate
 
   def service_worker; end
   def manifest; end
