17161a579b39e840ef81e1a78efa9544fe308313
Gabriel S. <thesouzagab@gmail.com>
2023-09-21 20:14:21 -0300

    feat: add web push subscription on service worker companion

--- app/javascript/custom/companion.js
+++ app/javascript/custom/companion.js
@@ -1,12 +1,47 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/service-worker.js", {scope: "/"})
-    .then(() => navigator.serviceWorker.ready)
-    .then((registration) => {
-      if ("SyncManager" in window) {
-        registration.sync.register("sync-forms");
-      } else {
-        window.alert("This browser does not support background sync.")
-      }
-    })
-    .then(() => {});
+async function registerServiceWorker() {
+  if (!navigator.serviceWorker) return
+
+  try {
+    await navigator.serviceWorker.register("/service-worker.js", { scope: "/" })
+  } catch (e) {
+    console.error("Service worker registration failed", e)
+  }
 }
+
+async function requestNotificationPermission() {
+  if (!("Notification" in window)) return false
+
+  const result = await Notification.requestPermission()
+
+  return result === "granted"
+}
+async function startServiceWorker(){
+  await registerServiceWorker()
+
+  if (await requestNotificationPermission()) {
+    const subscription = await getPushSubscription()
+
+    return sendSubscriptionToServer(subscription)
+  }
+}
+
+async function getPushSubscription() {
+  const registration = await navigator.serviceWorker.getRegistration()
+  if (!registration) return
+
+  const publicKey = document.head.querySelector('meta[name="vapid_public_key"]').getAttribute("content")
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: publicKey
+  })
+
+  return subscription
+}
+
+async function sendSubscriptionToServer(subscription) {
+  // TODO: post request to save subscription
+  console.table(subscription)
+}
+
+startServiceWorker()
+  .then(() => { console.log("Service Worker Started")})
