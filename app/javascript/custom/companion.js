c816a3a59279ed34d2cfca7ac2ca5839ad075f3a
Gabriel S <thesouzagab@gmail.com>
2023-10-02 20:08:02 -0300

    chore: wrap everything up

--- app/javascript/custom/companion.js
+++ app/javascript/custom/companion.js
@@ -1,59 +1,78 @@
 async function registerServiceWorker() {
-  if (!navigator.serviceWorker) return
+  if (!navigator.serviceWorker) return;
 
   try {
-    await navigator.serviceWorker.register("/service-worker.js", { scope: "/" })
+    await navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
   } catch (e) {
-    console.warn("Service worker registration failed", e)
-    throw e
+    console.warn("Service worker registration failed", e);
+    throw e;
   }
 }
 
 async function requestNotificationPermission() {
   if (!("Notification" in window)) return false
 
-  const result = await Notification.requestPermission()
-
-  return result === "granted"
+  try {
+    const result = await Notification.requestPermission();
+    return result === "granted"
+  } catch (e) {
+    console.warn("Notification permission request failed", e);
+    throw e
+  }
 }
-async function startServiceWorker(){
-  await registerServiceWorker()
 
-  if (await requestNotificationPermission()) {
-    const subscription = await getPushSubscription()
-    if (!subscription) return console.warn("No subscription active")
+async function startServiceWorker() {
+  try {
+    await registerServiceWorker()
 
-    await sendSubscriptionToServer(subscription)
+    if (await requestNotificationPermission()) {
+      const subscription = await getPushSubscription();
+      if (!subscription) {
+        console.warn("No subscription active");
+        return
+      }
+
+      await sendSubscriptionToServer(subscription);
+    }
+  } catch (e) {
+    console.warn("Service Worker initialization failed", e)
   }
 }
 
 async function getPushSubscription() {
-  const registration = await navigator.serviceWorker.getRegistration()
+  const registration = await navigator.serviceWorker.getRegistration();
   if (!registration) return
 
-  const publicKey = document.head.querySelector('meta[name="vapid_public_key"]').getAttribute("content")
+  const publicKey = document.head.querySelector('meta[name="vapid_public_key"]').getAttribute("content");
   const subscription = await registration.pushManager.subscribe({
     userVisibleOnly: true,
-    applicationServerKey: publicKey
+    applicationServerKey: publicKey,
   })
 
   return subscription
 }
 
 async function sendSubscriptionToServer(subscription) {
-  return fetch("/devices", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRF-Token": document.head.querySelector('meta[name="csrf-token"]').getAttribute("content")
-    },
-    body: JSON.stringify({ device: subscription.toJSON() })
-  })
+  try {
+    return fetch("/devices", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": document.head.querySelector('meta[name="csrf-token"]').getAttribute("content"),
+      },
+      body: JSON.stringify({ device: subscription.toJSON() }),
+    })
+  } catch (e) {
+    console.warn("Failed to send subscription to server", e);
+    throw e
+  }
 }
 
-
-try {
-  startServiceWorker().then(() => { console.log("Service Worker Started") })
-} catch (e) {
-  console.warn("Service worker registration failed", e)
-}
+(async () => {
+  try {
+    await startServiceWorker();
+    console.log("Service Worker Started");
+  } catch (e) {
+    console.warn("Service Worker initialization failed", e);
+  }
+})()
