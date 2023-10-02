fb84c6326576e2a6b4b4b9098b1e5ef4c447c7af
Gabriel S. <thesouzagab@gmail.com>
2023-10-02 19:07:20 -0300

    chore: add on push listener

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -31,20 +31,33 @@ function onInstall(event) {
 function onActivate(event) {
 
 
-    event.waitUntil(caches.keys().then((keys) => {
-        return Promise.all(keys.map((key) => caches.delete(key)))
-    }))
+    // event.waitUntil(caches.keys().then((keys) => {
+    //     return Promise.all(keys.map((key) => caches.delete(key)))
+    // }))
 }
 
 function onFetch(event) {
     // console.log('[Service-worker]', "Fetching!", event);
 }
 
-self.addEventListener('activate', () => {
-  clients.claim()
-})
+function onPush(event) {
+    const message = event.data.json()
+    const title = message.title || 'Yay a message'
+    const options = {
+        body: message.body || 'We have received a push message.',
+        icon: message.icon,
+        data: { url: message.url }
+    }
 
+    event.waitUntil(self.registration.showNotification(title, options))
+}
+function onNotificationClick(event) {
+  event.notification.close()
+  event.waitUntil(clients.openWindow(event.notification.data.url))
+}
+
+self.addEventListener('activate', onActivate)
 self.addEventListener('install', onInstall)
-
-
 self.addEventListener('fetch', onFetch)
+self.addEventListener('push', onPush)
+self.addEventListener('notificationclick', onNotificationClick)
